a8a90ce00a29f7f982b8cd30b568bfef1702d669
Vidya-kokkada <vidyakokkada7349@gmail.com>
2024-07-30 21:43:32 +0530

    Embroidery hoops

---
+++ footer.php
@@ -0,0 +1,32 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p>  +91-7349578029 </p>
+		 <p> sushmitha2002@gmail.com </p>
+		 <p>  vidya7349@gmail.com  </p>
+         <p>  Mangalore,India - 575001 </p>
+      </div>
+	  
+	   <div class="box">
+         <h3>follow us</h3>
+          <a href="https://www.instagram.com/__happy__hoops__?igsh=aXl4azIwd2c5cTk2">happy_hoops</a>
+      </div>
+
+   </div>
+</section>
---
+++ header.php
@@ -0,0 +1,47 @@
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
+
+<header class="header">
+
+   <div class="header-1">
+      <div class="flex">
+         <div class="share">
+            <a href="#" class="fab fa-instagram"></a>
+           
+         </div>
+		          <div class="user-box">
+            <p>username : <span><?php echo $_SESSION['user_name']; ?></span></p>
+            <p>email : <span><?php echo $_SESSION['user_email']; ?></span></p>
+            <a href="logout.php" class="delete-btn">logout</a>
+         </div>
+      </div>
+   </div>
+
+   <div class="header-2">
+      <div class="flex">
+         <a href="home.php" class="logo">Happy Hoops</a>
+
+         <nav class="navbar">
+            <a href="home.php">home</a>
+            <a href="shop.php">shop</a>
+            <a href="contact.php">contact</a>
+			 
+         </nav>
+
+         <div class="icons">
+            <div id="menu-btn" class="fas fa-bars"></div>
+            <a href="search_page.php" class="fas fa-search"></a>
+            <div id="user-btn" class="fas fa-user"></div>
+            
+
+</header>
